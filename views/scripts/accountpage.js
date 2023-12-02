897883f706b7200900d27e64263115d89d59ab21
SamiraMahyo <131866731+SamiraMahyo@users.noreply.github.com>
2023-12-02 20:38:58 +0100

    Styling my acc page

--- views/scripts/accountpage.js
+++ views/scripts/accountpage.js
@@ -7,7 +7,7 @@
 async function getUserInfo() {
     const user = await getUser(userAuth);
     //Change greeting on account page
-    document.getElementById("accountHeader").innerHTML = "Welcome back, " + user.name + "!";
+    document.getElementById("accountHeader").innerHTML = "Welcome back, " + user.name + "!😎☕️";
     //Fill in user info
     document.getElementById("accountname").value = user.name;
     document.getElementById("accountemail").value = user.email;
@@ -27,6 +27,39 @@ document.getElementById("accountbtn").addEventListener("click", function() {
     //Display a new button to save the changes
     document.getElementById("accountbtn").style.display = "none";
     document.getElementById("accountsavebtn").style.display = "block";
+    //Add eventlistener to the password field; If the user clicks on it, the field will be cleared, and if the user enters a new password, a confirm new password field will appear
+document.getElementById("accountpassword").addEventListener("click", function() {
+    document.getElementById("accountpassword").value = "";
+    document.getElementById("accountpassword").type = "password";
+    document.getElementById("accountpasswordconfirm").style.display = "block";
+    document.getElementById("confirmlabel").innerHTML = "Confirm new password:";
+    //remove the save button until the user has confirmed the new password
+    document.getElementById("accountsavebtn").style.display = "none";
+     //if the password and confirm password fields are not empty, and the password and confirm password fields match, the save button will reappear
+    document.getElementById("accountpasswordconfirm").addEventListener("keyup", function() {
+    if (document.getElementById("accountpassword").value == document.getElementById("accountpasswordconfirm").value) {
+        document.getElementById("accountsavebtn").style.display = "block";
+    }
+    else {
+        document.getElementById("accountsavebtn").style.display = "none";
+    }
+});
+    //add a cancel password change button
+    document.getElementById("accountcancelbtn").style.display = "block";
+    //add eventlistener to cancel password change button
+    document.getElementById("accountcancelbtn").addEventListener("click", function() {
+        document.getElementById("accountpassword").value = "xxxxxxxx";
+        document.getElementById("accountpasswordconfirm").style.display = "none";
+        document.getElementById("accountcancelbtn").style.display = "none";
+        document.getElementById("accountsavebtn").style.display = "none";
+        document.getElementById("confirmlabel").innerHTML = "";
+        document.getElementById("accountbtn").style.display = "block";
+        document.getElementById("accountpassword").readOnly = true;
+        document.getElementById("accountname").readOnly = true;
+        document.getElementById("accountemail").readOnly = true;
+        document.getElementById("accountphone").readOnly = true;
+    });
+});
 });
 
 
@@ -86,34 +119,7 @@ document.getElementById("accountsavebtn").addEventListener("click", function() {
     document.getElementById("accountsavebtn").style.display = "none";
 });
 
-//Add eventlistener to the password field; If the user clicks on it, the field will be cleared, and if the user enters a new password, a confirm new password field will appear
-document.getElementById("accountpassword").addEventListener("click", function() {
-    document.getElementById("accountpassword").value = "";
-    document.getElementById("accountpassword").type = "password";
-    document.getElementById("accountpasswordconfirm").style.display = "block";
-    document.getElementById("confirmlabel").innerHTML = "Confirm new password:";
-    //remove the save button until the user has confirmed the new password
-    document.getElementById("accountsavebtn").style.display = "none";
-     //if the password and confirm password fields are not empty, and the password and confirm password fields match, the save button will reappear
-    document.getElementById("accountpasswordconfirm").addEventListener("keyup", function() {
-    if (document.getElementById("accountpassword").value == document.getElementById("accountpasswordconfirm").value) {
-        document.getElementById("accountsavebtn").style.display = "block";
-    }
-    else {
-        document.getElementById("accountsavebtn").style.display = "none";
-    }
-});
-    //add a cancel password change button
-    document.getElementById("accountcancelbtn").style.display = "block";
-    //add eventlistener to cancel password change button
-    document.getElementById("accountcancelbtn").addEventListener("click", function() {
-        document.getElementById("accountpassword").value = "xxxxxxxx";
-        document.getElementById("accountpassword").type = "text";
-        document.getElementById("accountpasswordconfirm").style.display = "none";
-        document.getElementById("accountcancelpassword").style.display = "none";
-        document.getElementById("accountsavebtn").style.display = "none";
-    });
-});
+
 
     
 
