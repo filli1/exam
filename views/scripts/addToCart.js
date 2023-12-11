5475d8106fe43aaa7af6c6dd036c11122d7a17b9
Fillip Skole Andreasen <fillipskole@gmail.com>
2023-12-11 14:57:51 +0100

    made cart function better

--- views/scripts/addToCart.js
+++ views/scripts/addToCart.js
@@ -46,8 +46,62 @@ function addToCartFunction(product) {
 
     sessionStorage.setItem('cart', JSON.stringify(cart)); // Store updated cart
 
+    animateProductToCart(product.id);
+}
+
+function animateProductToCart(productId) {
+    var productElement = document.getElementById(`product${productId}`);
+    var productImg = productElement.querySelector('img');
+    var cartIcon = document.getElementById("cart-button");
+
+    if (productImg && cartIcon) {
+        var clone = productImg.cloneNode(true);
+        var position = productImg.getBoundingClientRect(); // Position relative to viewport
+        var cartPosition = cartIcon.getBoundingClientRect(); // Position relative to viewport
+
+        // Clone's starting position (relative to viewport)
+        clone.style.position = 'fixed';
+        clone.style.left = `${position.left}px`;
+        clone.style.top = `${position.top}px`;
+        clone.style.width = `${position.width}px`;
+        clone.style.height = `${position.height}px`;
+        clone.style.zIndex = 1001; // Above the sticky navbar
+
+        document.body.appendChild(clone);
+
+        // Calculate the translation to center the image in the middle of the cart icon
+        var translateX = cartPosition.left - position.left + (cartPosition.width / 2) - (position.width / 2);
+        var translateY = cartPosition.top - position.top + (cartPosition.height / 2) - (position.height / 2);
+
+
+        // Animate clone
+        clone.animate([
+            { transform: 'translate(0, 0) scale(1)' },
+            { transform: `translate(${translateX}px, ${translateY}px) scale(0.3)`, opacity: 0 }
+        ], {
+            duration: 1000,
+            easing: 'ease-in'
+        }).finished.then(() => {
+            clone.remove();
+            jiggleCart();
+        });
+    }
+
     updateCartCountDisplay();
 }
+
+function jiggleCart() {
+    var cartIcon = document.getElementById("cart-button");
+    if (cartIcon) {
+        cartIcon.classList.add('jiggle');
+
+        // Remove the class after the animation completes
+        setTimeout(() => {
+            cartIcon.classList.remove('jiggle');
+        }, 500); // Assuming jiggle animation duration is 500ms
+    }
+}
+
 
 // Update cart count display on page load
 document.addEventListener('DOMContentLoaded', updateCartCountDisplay);
--- views/scripts/cart.js
+++ views/scripts/cart.js
@@ -23,39 +23,59 @@ function getCart() {
     return cart ? JSON.parse(cart) : {};
 }
 
-let lineItems = [];
+lineItems = [];
 
 function displayCart() {
     const cart = getCart();
+    const numberOfDistinctProducts = Object.keys(cart).length;
     const cartOverview = document.getElementById('cart-overview');
 
-    // Create a table element
-    const cartTable = document.createElement('table');
-    cartOverview.appendChild(cartTable);
+    // If the cart is empty, display a message and return
+    if (numberOfDistinctProducts === 0) {
+        const p = document.createElement('p');
+        p.textContent = 'Your cart is empty.';
+        cartOverview.appendChild(p);
 
-    // Optionally, create and append a header row to the table
-    const headerRow = document.createElement('tr');
-    ['Image', 'Name', 'Quantity', 'Price', 'Total'].forEach(text => {
-        const th = document.createElement('th');
-        th.textContent = text;
-        headerRow.appendChild(th);
-    });
-    cartTable.appendChild(headerRow);
+        checkoutButton = document.querySelector('.checkout-button');
+        checkoutButton.style.display = 'none';
+        return;
+    } else {
 
-    // Append product rows to the table
-    Object.keys(cart).forEach(productId => {
-        fetch(`/products/${productId}`)
-            .then(response => response.json())
-            .then(product => {
-                lineItems.push({
-                    price: product.stripe_price_id,
-                    quantity: cart[productId].quantity,
+        // Create a table element
+        const cartTable = document.createElement('table');
+        cartOverview.appendChild(cartTable);
+
+        // Optionally, create and append a header row to the table
+        const headerRow = document.createElement('tr');
+        ['Image', 'Name', 'Quantity', 'Price', 'Total',''].forEach(text => {
+            const th = document.createElement('th');
+            th.textContent = text;
+            headerRow.appendChild(th);
+        });
+        cartTable.appendChild(headerRow);
+
+        // Append product rows to the table
+        Object.keys(cart).forEach(productId => {
+            console.log(productId);
+            fetch(`/products/${productId}`)
+                .then(response => response.json())
+                .then(product => {
+                    lineItems.push({
+                        price: product.stripe_price_id,
+                        quantity: cart[productId].quantity,
+                    })
+                    const productElement = createProductElement(product, cart[productId].quantity);
+                    cartTable.appendChild(productElement);
                 })
-                const productElement = createProductElement(product, cart[productId].quantity);
-                cartTable.appendChild(productElement);
-            })
-            .catch(error => console.error('Error fetching product:', error));
-    });
+                .catch(error => console.error('Error fetching product:', error));
+        });
+
+
+        // Display cart total
+        const totalDisplay = document.createElement('p');
+        cartOverview.appendChild(totalDisplay);
+        updateCartTotal();
+    }
 }
 
 //This takes the customer to the Stripe checkout page via the api endpoint on the server
@@ -81,6 +101,7 @@ function checkout() {
 function createProductElement(product, quantity) {
     // Create a table row
     const tr = document.createElement('tr');
+    tr.setAttribute('id', `row-${product.id}`);
     
     // Create and append the product image cell
     const imageCell = document.createElement('td');
@@ -99,18 +120,109 @@ function createProductElement(product, quantity) {
 
     // Create and append the quantity cell
     const quantityCell = document.createElement('td');
-    quantityCell.textContent = quantity;
+    const quantityDisplay = document.createElement('span');
+    quantityDisplay.textContent = quantity;
+    quantityDisplay.setAttribute('id', `quantity-${product.id}`);
+    quantityCell.appendChild(quantityDisplay);
+    
+
+    // Plus Button
+    const plusBtn = document.createElement('button');
+    plusBtn.textContent = '+';
+    plusBtn.onclick = () => adjustQuantity(product.id, 1);
+    quantityCell.appendChild(plusBtn);
+
+    // Minus Button
+    const minusBtn = document.createElement('button');
+    minusBtn.textContent = '-';
+    minusBtn.onclick = () => adjustQuantity(product.id, -1);
+    quantityCell.appendChild(minusBtn);
+
     tr.appendChild(quantityCell);
 
-    // Create and append the price cell
-    const priceCell = document.createElement('td');
-    priceCell.textContent = `${product.price.toFixed(2)} kr.`;
-    tr.appendChild(priceCell);
+     // Create and append the price cell
+     const priceCell = document.createElement('td');
+     priceCell.textContent = `${product.price.toFixed(2)} kr.`;
+     tr.appendChild(priceCell);
+ 
+     // Create and append the total price cell
+     const totalPriceCell = document.createElement('td');
+     totalPriceCell.setAttribute('id', `total-price-${product.id}`);
+     totalPriceCell.textContent = `${(product.price * quantity).toFixed(2)} kr.`;
+     tr.appendChild(totalPriceCell);
 
-    // Create and append the total price cell
-    const totalPriceCell = document.createElement('td');
-    totalPriceCell.textContent = `${(product.price * quantity).toFixed(2)} kr.`;
-    tr.appendChild(totalPriceCell);
+    // Delete Button
+    const deleteCell = document.createElement('td');
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'Delete';
+    deleteBtn.onclick = () => deleteItem(product.id);
+    deleteCell.appendChild(deleteBtn);
+    tr.appendChild(deleteCell);
 
     return tr;
+}
+
+function adjustQuantity(productId, adjustment) {
+    const cart = getCart();
+    if (cart[productId]) {
+        cart[productId].quantity += adjustment;
+
+        if (cart[productId].quantity <= 0) {
+            delete cart[productId];
+            document.getElementById(`row-${productId}`).remove(); // Remove the row for this product
+        } else {
+            const quantityDisplay = document.getElementById(`quantity-${productId}`);
+            if (quantityDisplay) {
+                quantityDisplay.textContent = cart[productId].quantity; // Update the quantity in the DOM
+            }
+            const totalPriceDisplay = document.getElementById(`total-price-${productId}`);
+            if (totalPriceDisplay) {
+                totalPriceDisplay.textContent = `${(cart[productId].price * cart[productId].quantity).toFixed(2)} kr.`; // Update the total price in the DOM
+            }
+        }
+
+        sessionStorage.setItem('cart', JSON.stringify(cart));
+        updateCartTotal(); // Update the cart total
+        updateCartCountDisplay(); // Update the cart count
+    }
+}
+
+
+function deleteItem(productId) {
+    const cart = getCart();
+    delete cart[productId];
+    sessionStorage.setItem('cart', JSON.stringify(cart));
+    updateCartDisplay();
+}
+
+function updateCartDisplay() {
+    document.getElementById('cart-overview').innerHTML = ''; // Clear existing content
+    displayCart();
+    updateCartCountDisplay(); // Update the cart count
+}
+
+function updateLineItems() {
+    const cart = getCart();
+    lineItems = Object.keys(cart).map(productId => ({
+        price: cart[productId].stripe_price_id,
+        quantity: cart[productId].quantity,
+    }));
+}
+
+function updateCartTotal() {
+    const cart = getCart();
+    let total = 0;
+    Object.values(cart).forEach(item => {
+        total += item.price * item.quantity;
+    });
+
+    const totalDisplay = document.getElementById('cart-total');
+    if (totalDisplay) {
+        totalDisplay.textContent = `Total: ${total.toFixed(2)} kr.`;
+    } else {
+        const newTotalDisplay = document.createElement('p');
+        newTotalDisplay.setAttribute('id', 'cart-total');
+        newTotalDisplay.textContent = `Total: ${total.toFixed(2)} kr.`;
+        document.getElementById('cart-overview').appendChild(newTotalDisplay);
+    }
 }
